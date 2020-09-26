89f4424f344fc4952e80bd753cd86751f1ccb978
Gaelsk <gaeltaf@gmail.com>
2020-09-26 10:35:30 +0200

    3

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const helmet = require("helmet");
+//const helmet = require("helmet");
 const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
@@ -37,7 +37,7 @@ const auth = require("./utils/auth");
 const admin = require("./utils/admin");
 
 //middlewares
-app.use(helmet());
+//app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
